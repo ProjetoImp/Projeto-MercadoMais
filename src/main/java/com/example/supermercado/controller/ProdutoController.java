c165811288ae72648d6d8d2a8ecbcaed01f8af57
Arilson Filadelfo <arilson.sousa2014@gmail.com>
2026-03-03 10:18:08 +0100

    alteração no ProdutoController para futura integração com o front

--- src/main/java/com/example/supermercado/controller/ProdutoController.java
+++ src/main/java/com/example/supermercado/controller/ProdutoController.java
@@ -3,6 +3,7 @@ package com.example.supermercado.controller;
 
 import com.example.supermercado.entity.Produto;
 import com.example.supermercado.service.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,8 +11,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
+@CrossOrigin(origins = "*")
 public class ProdutoController {
 
+    @Autowired
     private final ProdutoService produtoService;
 
     public ProdutoController(ProdutoService produtoService) {
